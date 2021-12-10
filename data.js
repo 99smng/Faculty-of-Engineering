ba173485ce82b3769804abb9c679b5895d0bea38
99smng <95893758+99smng@users.noreply.github.com>
2021-12-10 11:06:27 +0800

    first commit

---
+++ data.js
@@ -0,0 +1,138 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-along-engineering-drive-1",
+      "name": "Along Engineering Drive 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.418343544932279,
+        "pitch": 0.07225825590374768,
+        "fov": 1.19538726259933
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.48377124883298,
+          "pitch": 0.07225825590374768,
+          "rotation": 0,
+          "target": "1-engineering-auditorium-atrium"
+        },
+        {
+          "yaw": 0.41865999855910196,
+          "pitch": 0.26245393153672936,
+          "rotation": 0,
+          "target": "0-along-engineering-drive-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-engineering-auditorium-atrium",
+      "name": "Engineering Auditorium Atrium",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -1.1588618737698617,
+        "pitch": -0.15491945072604807,
+        "fov": 1.19538726259933
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6174459611009322,
+          "pitch": 0.03707048132301338,
+          "rotation": 0,
+          "target": "0-along-engineering-drive-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.2389858789362975,
+          "pitch": 0.2647234476222664,
+          "title": "Engineering Auditorium&nbsp;",
+          "text": "Spacious study spaces provided for NUS student and staff to complete their work and rest.&nbsp;"
+        }
+      ]
+    },
+    {
+      "id": "2-engineering-auditorium",
+      "name": "Engineering Auditorium",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -1.248658820892521,
+        "pitch": -0.407626681665068,
+        "fov": 1.19538726259933
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3593840406918432,
+          "pitch": -0.27961071594458353,
+          "rotation": 0,
+          "target": "0-along-engineering-drive-1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Faculty of engineering",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
